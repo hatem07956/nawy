eb625310c3ad98a5df1f5b7dbc29c61c2979a589
Hatem <m7mdhatem007@gmail.com>
2026-04-29 20:59:44 +0300

    add tf module

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,23 +1,9 @@
-# Pull the image from Docker Hub
-resource "docker_image" "node_hello" {
-  name         = var.image_name
-  keep_locally = false
-}
+module "node_hello" {
+  source = "./modules/docker_app"
 
-# Run the container
-resource "docker_container" "node_hello" {
-  name  = var.container_name
-  image = docker_image.node_hello.image_id
-
-  restart = "unless-stopped"
-
-  ports {
-    internal = var.container_port
-    external = var.host_port
-  }
-
-  labels {
-    label = "app"
-    value = "node-hello"
-  }
+  # Override defaults or omit to use the defaults in the module
+  image_name     = "hatem95/node-hello:latest"
+  container_name = "node-hello-app"
+  container_port = 3000
+  host_port      = 3000   # ← change host port here without touching the module
 }
